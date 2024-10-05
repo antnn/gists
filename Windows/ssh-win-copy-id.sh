8247500be5090b6b921530314a3ab01bff1a9a04
Anton Valishin <anton@valishin.ru>
2024-10-05 08:14:18 +0300

    Create ssh-win-copy-id.sh

---
+++ Windows/ssh-win-copy-id.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+authorizedKey=$(cat ~/.ssh/id_rsa.pub)
+remotePowershell="powershell Add-Content -Force -Path C:\\ProgramData\\ssh\\administrators_authorized_keys -Value '$authorizedKey';icacls.exe \"C:\\ProgramData\\ssh\\administrators_authorized_keys\" /inheritance:r /grant \"*S-1-5-32-544:F\" /grant \"SYSTEM:F\""
+ssh Administrator@192.168.122.2 "$remotePowershell"
