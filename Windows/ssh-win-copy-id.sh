84a9f61d785a540d470872f77c2fbaaa09cdec92
Anton Valishin <anton@valishin.ru>
2024-10-05 08:18:33 +0300

    Update ssh-win-copy-id.sh

--- Windows/ssh-win-copy-id.sh
+++ Windows/ssh-win-copy-id.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copies ssh key to Windows
 authorizedKey=$(cat ~/.ssh/id_rsa.pub)
 remotePowershell="powershell Add-Content -Force -Path C:\\ProgramData\\ssh\\administrators_authorized_keys -Value '$authorizedKey';icacls.exe \"C:\\ProgramData\\ssh\\administrators_authorized_keys\" /inheritance:r /grant \"*S-1-5-32-544:F\" /grant \"SYSTEM:F\""
 ssh Administrator@192.168.122.2 "$remotePowershell"
